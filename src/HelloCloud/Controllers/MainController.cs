2dec8f9ed90f5e5b43b976d0c2b3a7a00fd7347e
glennc <glennc@microsoft.com>
2016-03-15 20:08:05 -0700

    add index for quick alive test, add hostname, remove dnx451

--- src/HelloCloud/Controllers/MainController.cs
+++ src/HelloCloud/Controllers/MainController.cs
@@ -1,24 +1,50 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 
 namespace HelloCloud.Controllers
 {
     [Route("/")]
-    public class ValuesController : Controller
+    public class MainController : Controller
     {
+        public MainController()
+        {
+        }
         // GET: api/values
         [HttpGet]
         public string Get()
         {
             var cloudName = Environment.GetEnvironmentVariable("CLOUD_NAME");
+
             if(string.IsNullOrEmpty(cloudName))
             {
                 cloudName = "an unknown cloud";
             }
-            return $"Hello from {cloudName}";
+
+            //Environment.MachineName will not work until RC2 of corefx.
+            //HOSTNAME is set inside any Docker image.
+            var hostName = Environment.GetEnvironmentVariable("HOSTNAME");
+
+            if(string.IsNullOrEmpty(hostName))
+            {
+                hostName = "an unknown host";
+            }
+
+            return $"Hello from {cloudName} on {hostName}";
+        }
+
+        private static string GetHostName()
+        {
+            var hostName = Environment.GetEnvironmentVariable("HOSTNAME");
+            if(string.IsNullOrEmpty(hostName))
+            {
+                hostName = Environment.GetEnvironmentVariable("COMPUTERNAME");
+            }
+
+            return hostName;
         }
     }
 }
