306155612601f3054563731b37497762be78808a
Maria Naggaga <maria.naggaga@live.ca>
2016-03-18 19:44:42 -0400

    Added model and mapping page for 3djs

---
+++ src/whereyouat/Models/Location.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace whereyouat.Models
+{
+    public class Location
+    {
+        public int loc_Id { get; set; }
+        public float longitude { get; set; }
+        public float latitude { get; set; }
+
+        public string city_name { get; set; }
+    }
+}
