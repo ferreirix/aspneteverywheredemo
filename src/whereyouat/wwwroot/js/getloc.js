9a004943a6532aca490845d96d5700b64ce3b76a
Maria Naggaga <maria.naggaga@live.ca>
2016-03-23 23:55:31 -0400

    Geo Added to index page
    Index - PUT ,  geoloc.js get and put

---
+++ src/whereyouat/wwwroot/js/getloc.js
@@ -0,0 +1,26 @@
+﻿var x = document.getElementById("demo");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(showPosition);
+    }
+    else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+    //x.innerHTML = "Latitude: " + lat + "<br>Longitude: " + lon;
+
+    var xhr = new XMLHttpRequest();
+    var params = "latitude=" + lat + "&longitude=" + lon;
+    console.log("Params: " + params);
+    xhr.open('PUT', 'http://localhost:5000/locations', true);
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhr.onreadystatechange = function () {
+        console.log("Response:" & this.responseText);
+    };
+    xhr.send(params);
+}
