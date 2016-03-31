eaac740068a2a3d823888cd07262042e4da54226
Scott Hanselman <scott@hanselman.com>
2016-03-31 00:17:07 -0700

    Changing storage to include ALL rows not just first 1000

--- src/whereyouat/Controllers/LocationController.cs
+++ src/whereyouat/Controllers/LocationController.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.OptionsModel;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table;
 using whereyouat.Models;
+using System.Threading;
 
 namespace whereyouat.Controllers
 {
@@ -48,30 +49,29 @@ namespace whereyouat.Controllers
         public async Task<JsonResult> GetLocations()
         {
             CloudStorageAccount storageAccount = CloudStorageAccount.Parse(_settings.LocationConnectionString);
-
             CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
-
             CloudTable table = tableClient.GetTableReference("locations");
-
             TableQuery<LocationEntity> query = new TableQuery<LocationEntity>();
 
-            var data = await table.ExecuteQuerySegmentedAsync(query, new TableContinuationToken());
+            var data = await table.ExecuteQueryAsync(query);
+
+            //var data = await table.ExecuteQuerySegmentedAsync(query, new TableContinuationToken());
 
             return Json(data.Select(x => new { latitude = x.latitude, longitude = x.longitude, cloud_name = x.cloud_name }));
         }
+
+      
 
         [HttpGet("/cloudcounts")]
         public async Task<JsonResult> GetCloudCounts()
         {
             CloudStorageAccount storageAccount = CloudStorageAccount.Parse(_settings.LocationConnectionString);
-
             CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
-
             CloudTable table = tableClient.GetTableReference("locations");
-
             TableQuery<LocationEntity> query = new TableQuery<LocationEntity>();
 
-            var data = await table.ExecuteQuerySegmentedAsync(query, new TableContinuationToken());
+            var data = await table.ExecuteQueryAsync(query);
+            //var data = await table.ExecuteQuerySegmentedAsync(query, new TableContinuationToken());
 
             var result =
                 data.GroupBy(x => new { x.cloud_name, x.container })
@@ -161,6 +161,27 @@ namespace whereyouat.Controllers
         }
 
         #endregion
+
+    }
+
+
+    public static class CloudHelpers
+    {
+        public static async Task<IList<T>> ExecuteQueryAsync<T>(this CloudTable table, TableQuery<T> query, CancellationToken ct = default(CancellationToken), Action<IList<T>> onProgress = null) where T : ITableEntity, new()
+        {
+            var items = new List<T>();
+            TableContinuationToken token = null;
+            do
+            {
+                TableQuerySegment<T> seg = await table.ExecuteQuerySegmentedAsync<T>(query, token);
+                token = seg.ContinuationToken;
+                items.AddRange(seg);
+                if (onProgress != null) onProgress(items);
+
+            } while (token != null && !ct.IsCancellationRequested);
+
+            return items;
+        }
 
     }
 }
