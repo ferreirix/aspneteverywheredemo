4461826f5f4c19aead386bd8c5b4ec33e24cf5e5
Steve Lasker <StevenLasker@Hotmail.com>
2016-03-30 19:01:20 -0700

    Add Load Testing APIs

--- src/whereyouat/Controllers/LocationController.cs
+++ src/whereyouat/Controllers/LocationController.cs
@@ -19,12 +19,6 @@ namespace whereyouat.Controllers
             _settings = options.Value;
         }
 
-        [HttpGet("/test")]
-        public string GetTestResponse()
-        {
-            return "We're fine. We're all fine here now, thank you... How are you?";
-        }
-        
         [HttpPut("/locations")]
         public async Task AddLocation([FromForm]Location location)
         {
@@ -61,8 +55,9 @@ namespace whereyouat.Controllers
 
             var data = await table.ExecuteQuerySegmentedAsync(query, new TableContinuationToken());
 
-            return Json(data.Select(x=> new { latitude= x.latitude, longitude = x.longitude, cloud_name = x.cloud_name }));
+            return Json(data.Select(x => new { latitude = x.latitude, longitude = x.longitude, cloud_name = x.cloud_name }));
         }
+
         [HttpGet("/cloudcounts")]
         public async Task<JsonResult> GetCloudCounts()
         {
@@ -76,12 +71,73 @@ namespace whereyouat.Controllers
 
             var data = await table.ExecuteQuerySegmentedAsync(query, new TableContinuationToken());
 
-            var result = 
+            var result =
                 data.GroupBy(x => new { x.cloud_name, x.container })
-                    .Select(g=> new {g.Key.cloud_name, g.Key.container, Count = g.Count()});
-        //var result = data.Select(x => new { could = x.cloud_name, hostname = x.host, timestampe = x.Timestamp, container = x.container, pkey = x.PartitionKey, rkey = x.RowKey });
+                    .Select(g => new { g.Key.cloud_name, g.Key.container, Count = g.Count() })
+                    .OrderBy(o => new { o.cloud_name, o.container, o.Count });
+            //var result = data.Select(x => new { could = x.cloud_name, hostname = x.host, timestampe = x.Timestamp, container = x.container, pkey = x.PartitionKey, rkey = x.RowKey });
             return Json(result);
         }
+
+
+        #region Testing APIs
+        [HttpGet("/test")]
+        public string GetTestResponse()
+        {
+            return "We're fine. We're all fine here now, thank you... How are you?";
+        }
+        #endregion
+        
+        #region LoadTesting APIs
+        [HttpGet("/addrandomlocation")]
+        public async Task AddRandomLocation()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(_settings.LocationConnectionString);
+
+            var tableClient = storageAccount.CreateCloudTableClient();
+
+            CloudTable table = tableClient.GetTableReference("locations");
+
+            await table.CreateIfNotExistsAsync();
+
+            var locEntity = new LocationEntity(_settings.Cloud_Name ?? "LoadTest");
+            Location location = new Location();
+            Random randomLat = new Random();
+            Random randomLong = new Random();
+
+
+            float rndLat = randomLat.Next(100) - 70;
+            float rndLong = randomLong.Next(0, 300) - 150;
+
+            location.latitude = rndLat;
+            location.longitude = rndLong;
+            locEntity.longitude = location.longitude.ToString();
+            locEntity.latitude = location.latitude.ToString();
+            locEntity.host = _settings.Docker_Host;
+            locEntity.container = _settings.HostName;
+
+            TableOperation insertOperation = TableOperation.Insert(locEntity);
+
+            await table.ExecuteAsync(insertOperation);
+        }
+
+        [HttpGet("/locationsbycloud")]
+        public async Task<JsonResult> GetLocationsByCloud(string cloudname)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(_settings.LocationConnectionString);
+
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            CloudTable table = tableClient.GetTableReference("locations");
+
+            TableQuery<LocationEntity> query = new TableQuery<LocationEntity>();
+
+            var data = await table.ExecuteQuerySegmentedAsync(query, new TableContinuationToken());
+
+            return Json(data.Where(w => w.cloud_name == cloudname)
+                .Select(x => new { latitude = x.latitude, longitude = x.longitude, cloud_name = x.cloud_name }));
+        }
+
         [HttpGet("/alldata")]
         public async Task<JsonResult> GetAllData()
         {
@@ -99,6 +155,8 @@ namespace whereyouat.Controllers
             //var result = data.Select(x => new { could = x.cloud_name, hostname = x.host, timestampe = x.Timestamp, container = x.container, pkey = x.PartitionKey, rkey = x.RowKey });
             return Json(result);
         }
+
+        #endregion
 
     }
 }
